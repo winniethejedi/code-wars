7f8285eb82699a8ea5bb8793cb2855adaf5a25bb
Craig Parker <mcraigparker123@gmail.com>
2018-07-16 18:44:41 -0600

    another kata

---
+++ Odd-Even String Sort.js
@@ -0,0 +1,50 @@
+// Given a string S. You have to return another string such that even-indexed and odd-indexed characters of S are grouped and groups are space-separated (see sample below)
+
+// Note: 
+// 0 is considered to be an even index. 
+// All input strings are valid with no spaces
+// input: 'CodeWars' output 'CdWr oeas'
+// S[0] = 'C'
+// S[1] = 'o'
+// S[2] = 'd'
+// S[3] = 'e'
+// S[4] = 'W'
+// S[5] = 'a'
+// S[6] = 'r'
+// S[7] = 's'
+// Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+// odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+// And the final string to return is 'Cdwr oeas'
+
+// Enjoy.
+
+// My first solution
+
+function sortMyString(S) {
+    const evens = [];
+    const odds = [];
+    for (let i = 0; i < S.length; i ++) {
+        if (i % 2 === 0) {
+            evens.push(S[i]);
+        }
+        else odds.push(S[i]);
+    }
+    return `${evens.join('')} ${odds.join('')}`;
+}
+
+// Best solution on site
+
+const sortMyString2 = s => {
+    let even = s.split('').filter((v, i) => i % 2 === 0).join('')
+    let odd = s.split('').filter((v, i) => i % 2 !== 0).join('')
+    return even + ' ' + odd
+}
+
+// Reduce solution on site
+
+function sortMyString3(s) {
+    return [...s].reduce(function(a, c, i) {
+      a[i&1] += c;
+      return a;
+    }, ['','']).join(' ');
+  }
